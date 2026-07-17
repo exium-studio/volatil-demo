9a72a53743d5b4af06e54998ce86e4b03d892d49
havidarazidev <hafidarazidev@gmail.com>
2026-07-17 17:20:11 +0700

    refactor: remove redundant eslint-disable comment in useToastHistory hook

--- src/design-system/components/toast/hooks/use-toast-history.ts
+++ src/design-system/components/toast/hooks/use-toast-history.ts
@@ -26,7 +26,6 @@ export function useToastHistory() {
       group,
       entries: entries.sort((a, b) => b.createdAt - a.createdAt),
     }));
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- `all` is a fresh array each render by design (lazy TTL prune)
   }, [all]);
 
   const { deleteOne, deleteMany, clear, markRead, markAllRead } =
