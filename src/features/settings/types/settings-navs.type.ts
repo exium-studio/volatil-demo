60b688efc8a164f494d85949031aaf85735ee412
havidarazidev <hafidarazidev@gmail.com>
2026-07-20 22:05:10 +0700

    refactor: update settings navigation structure to use recursive NavNode types and items array

--- src/features/settings/types/settings-navs.type.ts
+++ src/features/settings/types/settings-navs.type.ts
@@ -1,8 +1,5 @@
 // src/features/settings/types/settings-navs.type.ts
 
 import type { SETTINGS_NAVS_MAP } from "@/features/settings/constants/settings.navs";
-import type { NavGroupItem } from "@/shared/types/nav.type";
 
 export type SettingNavKey = keyof typeof SETTINGS_NAVS_MAP;
-
-export type SettingsNavGroupItem = NavGroupItem<SettingNavKey>;
