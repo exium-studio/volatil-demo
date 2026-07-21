2d19116928bf3fb103c8df0dc4322e6eca768b2d
havidarazidev <hafidarazidev@gmail.com>
2026-07-21 12:36:06 +0700

    feat: implement navigation sidebar with dynamic routing using new shared configuration types

--- src/design-system/components/shell/ui/gis-app-shell.tsx
+++ src/design-system/components/shell/ui/gis-app-shell.tsx
@@ -1,14 +1,18 @@
 // src/design-system/components/shell/ui/basic-app-shell.tsx
 
-"use client";
-
 import { Logo } from "@/design-system/components/branding/ui/logo";
 import { Button } from "@/design-system/components/button/ui/button";
 import { HStack, VStack } from "@/design-system/components/layout/ui/flex-box";
 import { PageContainer } from "@/design-system/components/layout/ui/page-container";
+import { VNavs } from "@/design-system/components/navigation/ui/v-navs";
 import type { GisAppShellProps } from "@/design-system/components/shell/types/gis-app-shell.type";
+import { P } from "@/design-system/components/typography/ui/p";
+import { APP } from "@/design-system/constants/_meta";
 import { SettingsTrigger } from "@/features/settings/components/settings";
-import { Outlet } from "@tanstack/react-router";
+import { APP_NAV_GROUPS } from "@/shared/constants/app.nav-groups.";
+import { APP_NAVS_MAP } from "@/shared/constants/app.navs";
+import type { AppNavKey } from "@/shared/types/app-navs.type";
+import { Outlet, useNavigate, useParams } from "@tanstack/react-router";
 
 export const GisAppShell = (props: GisAppShellProps) => {
   // Props
@@ -27,11 +31,41 @@ export const GisAppShell = (props: GisAppShellProps) => {
 };
 
 const SideBar = () => {
+  // Hooks
+  const navigate = useNavigate();
+  const params = useParams({
+    strict: false,
+  }) as {
+    activeAppNavKey: AppNavKey;
+  };
+
+  // Derived Values
+  const activeAppNavKey = params.activeAppNavKey;
+
   return (
-    <VStack>
-      <HStack align={"center"}>
+    <VStack className={"debug"} w={"240px"}>
+      <HStack align={"center"} justify={"center"} gap={1} p={4}>
         <Logo />
+
+        <P mr={1} fontSize={"lg"} fontWeight={"semibold"}>
+          {APP.title}
+        </P>
       </HStack>
+
+      <VNavs<AppNavKey>
+        groups={APP_NAV_GROUPS}
+        navs={APP_NAVS_MAP}
+        // expanded={false}
+        onNavClick={(key) => {
+          navigate({
+            to: ".",
+            resetScroll: false,
+            replace: !!activeAppNavKey,
+            search: (old) => ({ ...old, activeAppNavKey: key }),
+          });
+        }}
+        p={2}
+      />
     </VStack>
   );
 };
