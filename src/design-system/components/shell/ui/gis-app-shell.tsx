716b76e4466bd08b4c0f7a7a0198e860b3f14554
havidarazidev <hafidarazidev@gmail.com>
2026-07-22 12:38:10 +0700

    refactor: fix Collapsible prop naming and implement Splitter layout in GIS app shell

--- src/design-system/components/shell/ui/gis-app-shell.tsx
+++ src/design-system/components/shell/ui/gis-app-shell.tsx
@@ -2,14 +2,13 @@
 
 import { AtrLogo } from "@/design-system/components/branding/ui/atr-logo";
 import type { IconButtonProps } from "@/design-system/components/button/types/button.type";
-import {
-  Button,
-  IconButton,
-} from "@/design-system/components/button/ui/button";
+import { IconButton } from "@/design-system/components/button/ui/button";
 import { AppIcon } from "@/design-system/components/icon/ui/app-icon";
+import { Center } from "@/design-system/components/layout/ui/center";
 import { HStack, VStack } from "@/design-system/components/layout/ui/flex-box";
 import { AppPageContainer } from "@/design-system/components/layout/ui/page-container";
 import { Separator } from "@/design-system/components/layout/ui/separator";
+import { Splitter } from "@/design-system/components/layout/ui/splitter";
 import { NavLink } from "@/design-system/components/navigation/ui/link";
 import { NavButton } from "@/design-system/components/navigation/ui/nav";
 import { VNavs } from "@/design-system/components/navigation/ui/v-navs";
@@ -22,7 +21,6 @@ import { HEADER_H } from "@/design-system/constants/styles";
 import { useIsSmallViewport } from "@/design-system/hooks/use-is-small-viewport";
 import { useNavStore } from "@/design-system/stores/use-nav-store";
 import { useThemeStore } from "@/design-system/stores/use-theme-store";
-import { SettingsTrigger } from "@/features/settings/components/settings";
 import { APP_NAV_GROUPS } from "@/shared/constants/app.nav-groups.";
 import { APP_NAVS_MAP } from "@/shared/constants/app.navs";
 import { t } from "@/shared/libs/i18n";
@@ -42,19 +40,44 @@ export const GisAppShell = (props: GisAppShellProps) => {
 
   return (
     <AppPageContainer
-      flexDir={isSmallViewport ? "column-reverse" : "row"}
+      flexDir={isSmallViewport ? "column" : "row"}
       bg={"bg.canvas"}
       // p={2}
       {...restProps}
     >
       <SideBar />
 
-      <SettingsTrigger modalKey={"settings"} mt={"auto"}>
-        <Button>Settings</Button>
-      </SettingsTrigger>
-
-      <Outlet />
+      <MainContent />
     </AppPageContainer>
+  );
+};
+
+const MainContent = () => {
+  // Hooks
+  const isSmallViewport = useIsSmallViewport();
+
+  return (
+    <Splitter.Root
+      panels={[
+        { id: "a", minSize: 30 },
+        { id: "b", minSize: 30 },
+      ]}
+      defaultSize={[50, 50]}
+      orientation={isSmallViewport ? "vertical" : "horizontal"}
+      minH={"60"}
+    >
+      <Splitter.Panel id={"a"} bg={"bg.body"}>
+        <Outlet />
+      </Splitter.Panel>
+
+      <Splitter.ResizeTrigger id={"a:b"} />
+
+      <Splitter.Panel id={"b"}>
+        <Center boxSize={"full"} textStyle={"2xl"}>
+          Base map
+        </Center>
+      </Splitter.Panel>
+    </Splitter.Root>
   );
 };
 
