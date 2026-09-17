ad5ae2d327e47e132f89d83af061e52407ecd658
mitrekadev <mitreka@zohomail.com>
2026-09-17 09:50:36 +0700

    refactor: use full border radius for badge and remove theme store

--- src/design-system/components/typography/ui/badge.tsx
+++ src/design-system/components/typography/ui/badge.tsx
@@ -1,17 +1,17 @@
 // src/design-system/components/typography/ui/badge.tsx
 
 import type { BadgeProps } from "@/design-system/components/typography/types/badge.type";
-import { useThemeStore } from "@/design-system/stores/theme-store";
 import { Badge as ChakraBadge } from "@chakra-ui/react";
 
 export const Badge = (props: BadgeProps) => {
   // Stores
-  const { theme } = useThemeStore();
+  // const { theme } = useThemeStore();
 
   return (
     <ChakraBadge
       fontSize={"sm"}
-      rounded={`calc(${theme.radii.component} - 2px)`}
+      // rounded={`calc(${theme.radii.component} - 2px)`}
+      rounded={`full`}
       {...props}
     />
   );
